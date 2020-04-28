91c4b2532e6f254075a7b36826e3229de6276f29
Yasuo Saito <yasuosaito13@gmail.com>
2020-04-28 23:58:54 +0900

    create initial class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+export class HelloWorld {
+    name: string
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    hello(): string {
+        return `hello ${name}!!`;
+    }
+}
