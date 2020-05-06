ba12f6b7d242811d72df982448e11474adff8f31
SAITO Yasuo <saito_ya@works-hi.co.jp>
2020-05-06 17:32:48 +0900

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './object';
+export * from './element';
+export * from './block';
+
+export * from './surfaces/messages';
+export * from './surfaces/modal';
+export * from './surfaces/home-tab';
